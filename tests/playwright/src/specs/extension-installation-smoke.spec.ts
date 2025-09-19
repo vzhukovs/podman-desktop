4721f922270a458ea7ebb9bd93be8cfa00464a15
George Serban <78701208+serbangeorge-m@users.noreply.github.com>
2025-09-19 11:59:29 +0200

    chore(test): enable openshiftDockerExtension test (#13986)
    chore(test): enable test
    
    Signed-off-by: serbangeorge-m <serbangeorge.m@gmail.com>

--- tests/playwright/src/specs/extension-installation-smoke.spec.ts
+++ tests/playwright/src/specs/extension-installation-smoke.spec.ts
@@ -35,7 +35,6 @@ import { SettingsBar } from '../model/pages/settings-bar';
 import { WelcomePage } from '../model/pages/welcome-page';
 import { NavigationBar } from '../model/workbench/navigation';
 import { Runner } from '../runner/podman-desktop-runner';
-import { isWindows } from '../utility/platform';
 
 let pdRunner: Runner;
 let page: Page;
@@ -65,8 +64,6 @@ for (const {
   extensionFullName,
 } of extensionsInstallationSmokeList) {
   test.describe.serial(`Extension installation for ${extensionName}`, { tag: '@smoke' }, () => {
-    test.skip(extensionName === openshiftDockerExtension.extensionName && !!isWindows); // Currently timing out in azure cicd https://github.com/podman-desktop/e2e/issues/396
-
     test.beforeAll(async () => {
       await _startup(extensionLabel);
     });
