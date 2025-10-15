3d047c70823822ebb91666818d777840d5323034
Evzen Gasta <evzen.ml@seznam.cz>
2025-10-15 18:13:58 +0200

    feat(Icon): added support for img component
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>

--- packages/ui/src/lib/icons/Icon.svelte
+++ packages/ui/src/lib/icons/Icon.svelte
@@ -28,6 +28,8 @@ const IconComponent = icon;
     <!-- -icon for extension icons e.g. 'kind-icon' -->
     {#if icon.startsWith('fas fa-') || icon.startsWith('far fa-') || icon.endsWith('-icon')}
         <i class={`${icon} ${size} ${className}`} {role} {title}></i>
+    {:else}
+        <img src={icon} alt={title ?? ''} {role} class={className} style={typeof size === 'number' ? `width: ${size}px; height: ${size}px;` : ''} />
     {/if}
 {:else}
     {#if IconComponent && typeof IconComponent !== 'string' && !isFontAwesomeIcon(IconComponent)}
