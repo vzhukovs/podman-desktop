5672af8582e65fbd94f0e7b4f27d5cee25e7b9b3
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-10-22 16:53:13 +0200

    refactor(extensions: podman): adding win-related checks to WinPlatform (#14491)
    Signed-off-by: Simon Rey <sfbrey+eqqe@gmail.com>
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    Signed-off-by: Simon Rey <51708585+simonrey1@users.noreply.github.com>
    Co-authored-by: Simon Rey <51708585+simonrey1@users.noreply.github.com>

---
+++ extensions/podman/packages/extension/src/platforms/win-platform.spec.ts
@@ -0,0 +1,128 @@
+/*********************************************************************
+ * Copyright (C) 2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************/
+
+import type { CheckResult, ExtensionContext, TelemetryLogger } from '@podman-desktop/api';
+import * as extensionApi from '@podman-desktop/api';
+import { beforeEach, expect, test, vi } from 'vitest';
+
+import type { HyperVCheck } from '/@/checks/windows/hyperv-check';
+import type { HyperVPodmanVersionCheck } from '/@/checks/windows/hyperv-podman-version-check';
+import type { VirtualMachinePlatformCheck } from '/@/checks/windows/virtual-machine-platform-check';
+import type { WinBitCheck } from '/@/checks/windows/win-bit-check';
+import type { WinMemoryCheck } from '/@/checks/windows/win-memory-check';
+import type { WinVersionCheck } from '/@/checks/windows/win-version-check';
+import type { WSLVersionCheck } from '/@/checks/windows/wsl-version-check';
+import type { WSL2Check } from '/@/checks/windows/wsl2-check';
+
+import { WinPlatform } from './win-platform';
+
+vi.mock('@podman-desktop/api', () => ({ env: { isWindows: false } }));
+
+const EXTENSION_CONTEXT_MOCK = {} as ExtensionContext;
+const TELEMETRY_LOGGER_MOCK = {} as TelemetryLogger;
+
+const WIN_BIT_CHECK_MOCK = { execute: vi.fn() } as unknown as WinBitCheck;
+const WIN_VERSION_CHECK_MOCK = { execute: vi.fn() } as unknown as WinVersionCheck;
+const WIN_MEMORY_CHECK_MOCK = { execute: vi.fn() } as unknown as WinMemoryCheck;
+const HYPERV_PODMAN_VERSION_CHECK_MOCK = { execute: vi.fn() } as unknown as HyperVPodmanVersionCheck;
+const HYPERV_CHECK_MOCK = { execute: vi.fn() } as unknown as HyperVCheck;
+const VIRTUAL_MACHINE_PLATFORM_CHECK_MOCK = { execute: vi.fn() } as unknown as VirtualMachinePlatformCheck;
+const WSL_VERSION_CHECK_MOCK = { execute: vi.fn() } as unknown as WSLVersionCheck;
+const WSL2_CHECK_MOCK = { execute: vi.fn() } as unknown as WSL2Check;
+
+const SUCCESSFUL_CHECK_RESULT: CheckResult = { successful: true };
+const FAILED_CHECK_RESULT: CheckResult = { successful: false };
+
+let winPlatform: WinPlatform;
+
+beforeEach(() => {
+  winPlatform = new WinPlatform(
+    EXTENSION_CONTEXT_MOCK,
+    TELEMETRY_LOGGER_MOCK,
+    WIN_BIT_CHECK_MOCK,
+    WIN_VERSION_CHECK_MOCK,
+    WIN_MEMORY_CHECK_MOCK,
+    HYPERV_PODMAN_VERSION_CHECK_MOCK,
+    HYPERV_CHECK_MOCK,
+    VIRTUAL_MACHINE_PLATFORM_CHECK_MOCK,
+    WSL_VERSION_CHECK_MOCK,
+    WSL2_CHECK_MOCK,
+  );
+});
+
+test('isHyperVEnabled should return false if it is not a Windows environment', async () => {
+  vi.mocked(extensionApi.env).isWindows = false;
+
+  const hypervEnabled = await winPlatform.isHyperVEnabled();
+
+  expect(hypervEnabled).toBeFalsy();
+});
+
+test('isHyperVEnabled should return false if Hyper-V check fails', async () => {
+  vi.mocked(extensionApi.env).isWindows = true;
+
+  vi.mocked(HYPERV_CHECK_MOCK.execute).mockResolvedValue(FAILED_CHECK_RESULT);
+  vi.mocked(HYPERV_PODMAN_VERSION_CHECK_MOCK.execute).mockResolvedValue(FAILED_CHECK_RESULT);
+
+  const hypervEnabled = await winPlatform.isHyperVEnabled();
+
+  expect(hypervEnabled).toBeFalsy();
+});
+
+test('isHyperVEnabled should return true if all Hyper-V checks succeed', async () => {
+  vi.mocked(extensionApi.env).isWindows = true;
+
+  vi.mocked(HYPERV_CHECK_MOCK.execute).mockResolvedValue(SUCCESSFUL_CHECK_RESULT);
+  vi.mocked(HYPERV_PODMAN_VERSION_CHECK_MOCK.execute).mockResolvedValue(SUCCESSFUL_CHECK_RESULT);
+
+  const hypervEnabled = await winPlatform.isHyperVEnabled();
+
+  expect(hypervEnabled).toBeTruthy();
+});
+
+test('isWSLEnabled should return false if not on Windows', async () => {
+  vi.mocked(extensionApi.env).isWindows = false;
+
+  const wslEnabled = await winPlatform.isWSLEnabled();
+
+  expect(wslEnabled).toBeFalsy();
+});
+
+test('isWSLEnabled should return false if any WSL check fails', async () => {
+  vi.mocked(extensionApi.env).isWindows = true;
+
+  vi.mocked(VIRTUAL_MACHINE_PLATFORM_CHECK_MOCK.execute).mockResolvedValue(SUCCESSFUL_CHECK_RESULT);
+  vi.mocked(WSL_VERSION_CHECK_MOCK.execute).mockResolvedValue(FAILED_CHECK_RESULT);
+  vi.mocked(WSL2_CHECK_MOCK.execute).mockResolvedValue(SUCCESSFUL_CHECK_RESULT);
+
+  const wslEnabled = await winPlatform.isWSLEnabled();
+
+  expect(wslEnabled).toBeFalsy();
+});
+
+test('isWSLEnabled should return true if all WSL checks succeed', async () => {
+  vi.mocked(extensionApi.env).isWindows = true;
+
+  vi.mocked(VIRTUAL_MACHINE_PLATFORM_CHECK_MOCK.execute).mockResolvedValue(SUCCESSFUL_CHECK_RESULT);
+  vi.mocked(WSL_VERSION_CHECK_MOCK.execute).mockResolvedValue(SUCCESSFUL_CHECK_RESULT);
+  vi.mocked(WSL2_CHECK_MOCK.execute).mockResolvedValue(SUCCESSFUL_CHECK_RESULT);
+
+  const wslEnabled = await winPlatform.isWSLEnabled();
+
+  expect(wslEnabled).toBeTruthy();
+});
--- extensions/podman/packages/extension/src/platforms/win-platform.ts
+++ extensions/podman/packages/extension/src/platforms/win-platform.ts
@@ -16,9 +16,79 @@
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
 
-import { injectable } from 'inversify';
+import type { InstallCheck } from '@podman-desktop/api';
+import * as extensionApi from '@podman-desktop/api';
+import { inject, injectable } from 'inversify';
+
+import { OrCheck, SequenceCheck } from '/@/checks/base-check';
+import { HyperVCheck } from '/@/checks/windows/hyperv-check';
+import { HyperVPodmanVersionCheck } from '/@/checks/windows/hyperv-podman-version-check';
+import { VirtualMachinePlatformCheck } from '/@/checks/windows/virtual-machine-platform-check';
+import { WinBitCheck } from '/@/checks/windows/win-bit-check';
+import { WinMemoryCheck } from '/@/checks/windows/win-memory-check';
+import { WinVersionCheck } from '/@/checks/windows/win-version-check';
+import { WSLVersionCheck } from '/@/checks/windows/wsl-version-check';
+import { WSL2Check } from '/@/checks/windows/wsl2-check';
+import { ExtensionContextSymbol, TelemetryLoggerSymbol } from '/@/inject/symbols';
 
 @injectable()
 export class WinPlatform {
   readonly type = 'win';
+
+  private readonly windowsVirtualizationCheck: OrCheck;
+  private readonly wslCheck: SequenceCheck;
+  private readonly hyperVSequenceCheck: SequenceCheck;
+
+  constructor(
+    @inject(ExtensionContextSymbol)
+    readonly extensionContext: extensionApi.ExtensionContext,
+    @inject(TelemetryLoggerSymbol)
+    readonly telemetryLogger: extensionApi.TelemetryLogger,
+    @inject(WinBitCheck)
+    readonly winBitCheck: WinBitCheck,
+    @inject(WinVersionCheck)
+    readonly winVersionCheck: WinVersionCheck,
+    @inject(WinMemoryCheck)
+    readonly winMemoryCheck: WinMemoryCheck,
+    @inject(HyperVPodmanVersionCheck)
+    readonly hyperVPodmanVersionCheck: HyperVPodmanVersionCheck,
+    @inject(HyperVCheck)
+    readonly hyperVCheck: HyperVCheck,
+    @inject(VirtualMachinePlatformCheck)
+    readonly virtualMachinePlatformCheck: VirtualMachinePlatformCheck,
+    @inject(WSLVersionCheck)
+    readonly wSLVersionCheck: WSLVersionCheck,
+    @inject(WSL2Check)
+    readonly wSL2Check: WSL2Check,
+  ) {
+    this.hyperVSequenceCheck = new SequenceCheck('Hyper-V Platform', [this.hyperVPodmanVersionCheck, this.hyperVCheck]);
+
+    this.wslCheck = new SequenceCheck('WSL platform', [
+      this.virtualMachinePlatformCheck,
+      this.wSLVersionCheck,
+      this.wSL2Check,
+    ]);
+
+    this.windowsVirtualizationCheck = new OrCheck('Windows virtualization', this.wslCheck, this.hyperVSequenceCheck);
+  }
+
+  getPreflightChecks(): InstallCheck[] {
+    return [this.winBitCheck, this.winVersionCheck, this.winMemoryCheck, this.windowsVirtualizationCheck];
+  }
+
+  async isWSLEnabled(): Promise<boolean> {
+    if (!extensionApi.env.isWindows) {
+      return false;
+    }
+    const wslCheckResult = await this.wslCheck.execute();
+    return wslCheckResult.successful;
+  }
+
+  async isHyperVEnabled(): Promise<boolean> {
+    if (!extensionApi.env.isWindows) {
+      return false;
+    }
+    const hyperVCheckResult = await this.hyperVSequenceCheck.execute();
+    return hyperVCheckResult.successful;
+  }
 }
