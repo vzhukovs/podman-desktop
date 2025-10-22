c38dab339101f496ca1790d3d8d1bcfa61d501c9
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-10-22 16:47:31 +0200

    refactor(renderer: PodsList.svelte): adding key props to table usage (#14505)
    * refactor(renderer: PodsList.svelte): adding key props to table usage
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * chore: adding engineId in the key function
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/pod/PodsList.svelte
+++ packages/renderer/src/lib/pod/PodsList.svelte
@@ -154,6 +154,13 @@ const columns = [
 ];
 
 const row = new TableRow<PodInfoUI>({ selectable: (_pod): boolean => true });
+
+/**
+ * Utility function for the Table to get the key to use for each item
+ */
+function key(pod: PodInfoUI): string {
+  return `${pod.engineId}:${pod.id}`;
+}
 </script>
 
 <NavPage bind:searchTerm={searchTerm} title="pods">
@@ -250,6 +257,7 @@ const row = new TableRow<PodInfoUI>({ selectable: (_pod): boolean => true });
         row={row}
         defaultSortColumn="Name"
         enableLayoutConfiguration={true}
+        key={key}
         on:update={(): PodInfoUI[] => (pods = pods)}>
       </Table>
     {/if}
