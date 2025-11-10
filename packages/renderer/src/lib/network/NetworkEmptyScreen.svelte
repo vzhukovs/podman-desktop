cb0fdffb5176e5bf8c60dcf5e1d742101d0b1668
Sonia Sandler <66797193+SoniaSandler@users.noreply.github.com>
2025-11-10 14:35:30 -0500

    chore: update network icon in empty network page (#14814)
    Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/renderer/src/lib/network/NetworkEmptyScreen.svelte
+++ packages/renderer/src/lib/network/NetworkEmptyScreen.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 import { EmptyScreen } from '@podman-desktop/ui-svelte';
-import { ContainerIcon } from '@podman-desktop/ui-svelte/icons';
+
+import NetworkIcon from '/@/lib/images/NetworkIcon.svelte';
 
 const firstNetwork = 'my-first-network';
 const commandLine = `podman network create ${firstNetwork}`;
 </script>
 
 <EmptyScreen
-  icon={ContainerIcon}
+  icon={NetworkIcon}
   title="No networks"
   message="Create your first network using the following command line:"
   commandline={commandLine}
