78cf63b2421c1f919050daa3241224d8c1f452ec
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-11-24 09:53:14 +0100

    refactor(renderer/Label): migrate to Svelte5 (#15029)
    * refactor(renderer/Label): migrate to Svelte5
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix: adding default value for capitalize
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/ui/Label.svelte
+++ packages/renderer/src/lib/ui/Label.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
 import { Tooltip } from '@podman-desktop/ui-svelte';
+import type { Snippet } from 'svelte';
 
-export let name = '';
-export let tip = '';
-export let role: string | undefined = undefined;
-export let capitalize: boolean = false;
+interface Props {
+  name?: string;
+  tip?: string;
+  role?: string;
+  capitalize?: boolean;
+  children?: Snippet;
+}
+
+let { name = '', tip = '', role, capitalize = false, children }: Props = $props();
 </script>
 
 <Tooltip top tip={tip}>
   <div
     role={role}
     class="flex items-center bg-[var(--pd-label-bg)] p-1 rounded-md text-sm text-[var(--pd-label-text)] gap-x-1">
-    <slot></slot>
+    {@render children?.()}
     <span class:capitalize={capitalize}>
       {name}
     </span>
