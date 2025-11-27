ee95b2efcde9431f7b9d3fedb44f0412de60ea27
Florent Benoit <fbenoit@redhat.com>
2025-11-27 17:05:57 +0100

    chore: include product.json as a path alias
    path alias to avoid to import from ../../../../product.json
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/main/vite.config.js
+++ packages/main/vite.config.js
@@ -33,6 +33,7 @@ const config = {
     alias: {
       '/@/': join(PACKAGE_ROOT, 'src') + '/',
       '/@api/': join(PACKAGE_ROOT, '../api/src') + '/',
+      '/@product.json': `${join(PACKAGE_ROOT, '../../product.json')}`,
     },
   },
   build: {
