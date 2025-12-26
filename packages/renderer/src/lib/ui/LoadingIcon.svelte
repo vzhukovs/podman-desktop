8a154fdb03f31310c199a0689606bfbbeda6dd97
Evžen Gasta <46690189+gastoner@users.noreply.github.com>
2025-12-26 21:21:58 +0100

    refactor: refactored LoadingIcon to svelte5 (#15424)
    * refactor: migrated LoadingIcon component to svelte5
    
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>
    
    * chore: fixed linter
    
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>
    
    ---------
    
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>

--- packages/renderer/src/lib/ui/LoadingIcon.svelte
+++ packages/renderer/src/lib/ui/LoadingIcon.svelte
@@ -2,11 +2,21 @@
 import type { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { Fa, type IconSize } from 'svelte-fa';
 
-export let icon: IconDefinition;
-export let loadingWidthClass: string = 'w-6';
-export let loadingHeightClass: string = 'h-6';
-export let loading = false;
-export let iconSize: IconSize | undefined = undefined;
+interface Props {
+  icon: IconDefinition;
+  loadingWidthClass?: string;
+  loadingHeightClass?: string;
+  loading?: boolean;
+  iconSize?: IconSize;
+}
+
+let {
+  icon,
+  loadingWidthClass = 'w-6',
+  loadingHeightClass = 'h-6',
+  loading = false,
+  iconSize = undefined,
+}: Props = $props();
 </script>
 
 <div>
