31fc77dc8ed0c4c97d5540f390c33731af33831d
Charlie Drage <charlie@charliedrage.com>
2026-01-06 09:47:58 -0500

    chore(preferences): add readonly support to SliderItem component (#15552)
    ### What does this PR do?
    
    When record.readonly is true, the range slider input is now disabled,
    preventing user interaction with the slider.
    
    ### Screenshot / video of UI
    
    N/A, no (current) user facing change as it's more of a chore / align
    with how other preferences such as BooleanItem does it with disabled /
    readonly.
    
    ### What issues does this PR fix or reference?
    
    Part of https://github.com/podman-desktop/podman-desktop/issues/14624
    
    ### How to test this PR?
    
    - [X] Tests are covering the bug fix or the new feature
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/renderer/src/lib/preferences/item-formats/SliderItem.spec.ts
+++ packages/renderer/src/lib/preferences/item-formats/SliderItem.spec.ts
@@ -46,3 +46,21 @@ test('Ensure HTMLInputElement', async () => {
 
   expect(input instanceof HTMLInputElement).toBe(true);
 });
+
+test('Expect slider to be disabled when record.readonly is true', async () => {
+  const record: IConfigurationPropertyRecordedSchema = {
+    id: 'record',
+    title: 'record',
+    parentId: 'parent.record',
+    description: 'record-description',
+    type: 'number',
+    minimum: 4,
+    maximum: 34,
+    readonly: true,
+  };
+
+  render(SliderItem, { record, value: 15 });
+  const input = screen.getByLabelText('record-description');
+  expect(input).toBeInTheDocument();
+  expect(input).toBeDisabled();
+});
