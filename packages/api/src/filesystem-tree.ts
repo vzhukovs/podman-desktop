e70d087a2da9dcdad2397cd956c7ebe98970ed18
Florent Benoit <fbenoit@redhat.com>
2026-02-09 10:21:58 +0100

    refactor(filesystem-tree): add conditional assignment for optional property
    related to https://www.typescriptlang.org/tsconfig/#exactOptionalPropertyTypes
    
    I'll add stricter configuration in packages/api so fixing it before
    
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/api/src/filesystem-tree.ts
+++ packages/api/src/filesystem-tree.ts
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (C) 2024 Red Hat, Inc.
+ * Copyright (C) 2024-2026 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,7 +40,9 @@ export class FilesystemNode<T> {
 
   copy(): FilesystemNode<T> {
     const result = new FilesystemNode<T>(this.name, this.isDirectory);
-    result.data = this.data;
+    if (this.data !== undefined) {
+      result.data = this.data;
+    }
     result.size = this.size;
     result.hidden = this.hidden;
     for (const [name, child] of this.children) {
