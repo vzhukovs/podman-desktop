a49564aaa7560b2f016f7f01f724571eb393312f
Florent Benoit <fbenoit@redhat.com>
2026-02-11 10:49:12 +0100

    refactor(charCode): remove const keyword on the export
    it does not work with verbatimModuleSyntax option of typescript
    on the import side with 'const'
    
    https://www.typescriptlang.org/tsconfig/#verbatimModuleSyntax
    
    related to https://github.com/podman-desktop/podman-desktop/issues/15496
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/api/src/context/charCode.ts
+++ packages/api/src/context/charCode.ts
@@ -24,10 +24,9 @@
 // Names from https://blog.codinghorror.com/ascii-pronunciation-rules-for-programmers/
 
 /**
- * An inlined enum containing useful character codes (to be used with String.charCodeAt).
- * Please leave the const keyword such that it gets inlined when compiled to JavaScript!
+ * An enum containing useful character codes (to be used with String.charCodeAt).
  */
-export const enum CharCode {
+export enum CharCode {
   Null = 0,
   /**
    * The `\b` character.
