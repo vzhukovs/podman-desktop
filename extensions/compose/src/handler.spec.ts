e9700b40c9d43584cd59fac2ca389a6460788118
Florent BENOIT <fbenoit@redhat.com>
2026-02-11 14:33:53 +0100

    refactor(compose): fix the original intent of describe methods (#16191)
    there is a describe method in the middle of tests. It's probably
    related to an issue of a rebase.
    
    Just move the inner describe method to the root level
    
    newer vitest versions failed to run these tests
    
    related to https://github.com/podman-desktop/podman-desktop/pull/16182
    
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- extensions/compose/src/handler.spec.ts
+++ extensions/compose/src/handler.spec.ts
@@ -49,33 +49,33 @@ test('updateConfigAndContextComposeBinary: make sure configuration gets updated 
   await handler.updateConfigAndContextComposeBinary(extensionContextMock);
 
   expect(configUpdateSpy).toHaveBeenCalledWith('binary.installComposeSystemWide', true);
-
-  describe('podman-compose', async () => {
-    test('updateConfigAndContextComposeBinary: should set isPodmanComposeInstalledSystemWide context to true when podman-compose is installed', async () => {
-      vi.mocked(detect.Detect.prototype.checkSystemWideDockerCompose).mockResolvedValue(true);
-      vi.mocked(detect.Detect.prototype.checkSystemWidePodmanCompose).mockResolvedValue(true);
-
-      // Run updateConfigAndContextComposeBinary
-      await handler.updateConfigAndContextComposeBinary(extensionContextMock);
-
-      expect(vi.mocked(extensionApi.context.setValue)).toHaveBeenCalledWith(
-        'compose.isPodmanComposeInstalledSystemWide',
-        true,
-      );
-    });
-
-    test('updateConfigAndContextComposeBinary: should set isPodmanComposeInstalledSystemWide context to false when podman-compose is not installed', async () => {
-      vi.mocked(detect.Detect.prototype.checkSystemWideDockerCompose).mockResolvedValue(false);
-      vi.mocked(detect.Detect.prototype.checkSystemWidePodmanCompose).mockResolvedValue(false);
-
-      // Run updateConfigAndContextComposeBinary
-      await handler.updateConfigAndContextComposeBinary(extensionContextMock);
-
-      expect(vi.mocked(extensionApi.context.setValue)).toHaveBeenCalledWith(
-        'compose.isPodmanComposeInstalledSystemWide',
-        false,
-      );
-    });
-  });
   expect(contextUpdateSpy).toHaveBeenCalledWith('compose.isComposeInstalledSystemWide', true);
+});
+
+describe('podman-compose', async () => {
+  test('updateConfigAndContextComposeBinary: should set isPodmanComposeInstalledSystemWide context to true when podman-compose is installed', async () => {
+    vi.mocked(detect.Detect.prototype.checkSystemWideDockerCompose).mockResolvedValue(true);
+    vi.mocked(detect.Detect.prototype.checkSystemWidePodmanCompose).mockResolvedValue(true);
+
+    // Run updateConfigAndContextComposeBinary
+    await handler.updateConfigAndContextComposeBinary(extensionContextMock);
+
+    expect(vi.mocked(extensionApi.context.setValue)).toHaveBeenCalledWith(
+      'compose.isPodmanComposeInstalledSystemWide',
+      true,
+    );
+  });
+
+  test('updateConfigAndContextComposeBinary: should set isPodmanComposeInstalledSystemWide context to false when podman-compose is not installed', async () => {
+    vi.mocked(detect.Detect.prototype.checkSystemWideDockerCompose).mockResolvedValue(false);
+    vi.mocked(detect.Detect.prototype.checkSystemWidePodmanCompose).mockResolvedValue(false);
+
+    // Run updateConfigAndContextComposeBinary
+    await handler.updateConfigAndContextComposeBinary(extensionContextMock);
+
+    expect(vi.mocked(extensionApi.context.setValue)).toHaveBeenCalledWith(
+      'compose.isPodmanComposeInstalledSystemWide',
+      false,
+    );
+  });
 });
